a0ecc9027262bba83ce98222ad42ffd7cb0306f2
ktzar <miguelmail@gmail.com>
2012-06-06 23:40:39 +0100

    Encapsulate the code in script.js

--- js/script.js
+++ js/script.js
@@ -1,63 +1,90 @@
-var reader = new FileReader();
-reader.onload = function(e) {
-    var img = new Image();
-    //Create function that processes the image for when the image is loaded
-    img.onload = function(the_img) {
-        return function(){
-            var cnv = document.getElementById('editting_canvas');
-            var ctx = cnv.getContext('2d');
-            ctx.fillStyle = 'black';
-            ctx.fillRect(0,0,cnv.width, cnv.height);
-            //Calculate new proportions and offset
-            var new_height, new_width, ratio, x_offset, y_offset;
-            if ( the_img.width > the_img.height ) {
-                ratio = the_img.width / cnv.width;
-                new_height = the_img.height / ratio;
-                new_width = cnv.width;
-                x_offset = 0;
-                y_offset = (cnv.height - new_height)/2;
-            } else {
-                ratio = the_img.height / cnv.height;
-                new_height = cnv.height;
-                new_width = the_img.width / ratio;
-                y_offset = 0;
-                x_offset = (cnv.width - new_width)/2;
-            }
-            ctx.drawImage(the_img, x_offset, y_offset, new_width, new_height);
-        };
-    }(img);
-    img.src = e.target.result;
-}; 
+var Migstagram = function(){
 
-/**
- * Read the files from the element
- */
-function updatePic(files){
-    var newPic;
-    //Check that there are files
-    if (files && files.length>0) {
-        newPic = files[0];
-        //Check that it's a file
-        if (
-            newPic.type != "image/jpeg" &&
-            newPic.type != "image/jpg"  &&
-            newPic.type != "image/png"  &&
-            newPic.type != "image/gif"
-            ) {
-            alert ('Only images, please');
+    var that = this;
+    
+    var reader = new FileReader();
+    var cnv = document.getElementById('editting_canvas');
+    var ctx = cnv.getContext('2d');
+
+    var originalImageData;
+
+
+    reader.onload = function(e) {
+        var img = new Image();
+        //Create function that processes the image for when the image is loaded
+        img.onload = function(the_img) {
+            return function(){
+                ctx.fillStyle = 'black';
+                ctx.fillRect(0,0,cnv.width, cnv.height);
+                //Calculate new proportions and offset
+                var new_height, new_width, ratio, x_offset, y_offset;
+                if ( the_img.width > the_img.height ) {
+                    ratio = the_img.width / cnv.width;
+                    new_height = the_img.height / ratio;
+                    new_width = cnv.width;
+                    x_offset = 0;
+                    y_offset = (cnv.height - new_height)/2;
+                } else {
+                    ratio = the_img.height / cnv.height;
+                    new_height = cnv.height;
+                    new_width = the_img.width / ratio;
+                    y_offset = 0;
+                    x_offset = (cnv.width - new_width)/2;
+                }
+                ctx.drawImage(the_img, x_offset, y_offset, new_width, new_height);
+                originalImageData = ctx.getImageData(0,0,cnv.width,cnv.height);
+            };
+        }(img);
+        img.src = e.target.result;
+    }; 
+
+    /**
+     * Read the files from the element
+     */
+    this.updatePic = function(files){
+        var newPic;
+        //Check that there are files
+        if (files && files.length>0) {
+            newPic = files[0];
+            //Check that it's a file
+            if (
+                    newPic.type != "image/jpeg" &&
+                    newPic.type != "image/jpg"  &&
+                    newPic.type != "image/png"  &&
+                    newPic.type != "image/gif"
+               ) {
+                   alert ('Only images, please');
+                   return;
+               }
+            //Convert the pic to base64 url data
+            reader.readAsDataURL(newPic);
+
+        } else { //There were no files or FileReader is not supported
+            console.log("no files :'(");
             return;
         }
-        //Convert the pic to base64 url data
-        reader.readAsDataURL(newPic);
-
-    } else { //There were no files or FileReader is not supported
-        console.log("no files :'(");
-        return;
     }
-}
+
+    this.resetPicture = function()
+    {
+        var cnv = document.getElementById('editting_canvas');
+        var ctx = cnv.getContext('2d');
+        ctx.putImageData(originalImageData);
+
+    }
+};
 
 
 $(function(){
+    var myMigstagram;
+
+    $('#image_upload').on('change', function(e){
+        if (typeof e.target.files != "undefined") {
+            myMigstagram.updatePic(e.target.files);
+        }
+    });
+
+    myMigstagram = new Migstagram();
 
     //position the loading div on top of the canvas and hide it
     var canvas_position = ($('#editting_canvas').position());
